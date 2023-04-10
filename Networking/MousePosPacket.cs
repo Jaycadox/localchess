10b9397872bf1add9f90ee58e07ff0a4c391c8f7
Jaycadox <68955944+Jaycadox@users.noreply.github.com>
2023-04-11 01:19:12 +1000

    Broadcast mouse pos and packet compression

---
+++ Networking/MousePosPacket.cs
@@ -0,0 +1,34 @@
+﻿using localChess.Chess;
+
+namespace localChess.Networking
+{
+    internal class MousePosPacket : Packet
+    {
+        public int X { get; set; } = 0;
+        public int Y { get; set; } = 0;
+
+        public MousePosPacket()
+        {
+            Id = 5;
+        }
+
+        public void Handle(Game game)
+        {
+            if (Program.Gui!.TargetMouseX == X && Program.Gui!.TargetMouseY == Y)
+            {
+                return;
+            }
+            Program.Gui!.TargetMouseX = X;
+            Program.Gui!.TargetMouseY = Y;
+            if (Program.Gui!.MouseX == -1)
+            {
+                Program.Gui!.MouseX = X;
+            }
+            if (Program.Gui!.MouseY == -1)
+            {
+                Program.Gui!.MouseY = Y;
+            }
+            Program.Gui!.MouseOpacity = 255;
+        }
+    }
+}
